0231a14de82763c2df66b792eb0303cca5860a63
Dimitar.Petroww <Mitko66692@abv.bg>
2021-03-15 12:29:46 +0200

    Register, Home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,22 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+const template = () => html`
+<section id="home-page">
+    <div class="jumbotron jumbotron-fluid text-light" style="background-color: #343a40;">
+        <img src="https://slicksmovieblog.files.wordpress.com/2014/08/cropped-movie-banner-e1408372575210.jpg"
+            class="img-fluid" alt="Responsive image" style="width: 150%; height: 200px">
+        <h1 class="display-4">Movies</h1>
+        <p class="lead">Unlimited movies, TV shows, and more. Watch anywhere. Cancel anytime.</p>
+    </div>
+</section>
+`
+
 class Home extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = "Home"
+        this.render()
+    }
+    render() {
+        render(template(), this)
     }
 }
 
